72724f3f99cc78ac2205dcfce6e72978656cb757
tikuwa9030 <tikuwa9030@gmail.com>
2023-05-02 20:12:59 +0900

    文法エラーを更新
    format構文の{0:%P}→{0:%p}に更新。

--- degital-clock.py
+++ degital-clock.py
@@ -1,25 +1,25 @@
 import tkinter as tk
 import datetime as dt
 
-# datetimeで現在時刻を取得
 
-
+# def clock():
+# 曜日のリスト
 def clock():
-    # 曜日のリスト
     weeklist = {0: "月曜日", 1: "火曜日", 2: "水曜日",
                 3: "木曜日", 4: "金曜日", 5: "土曜日", 6: "日曜日"}
 
     now = dt.datetime.now()
     week1 = now.weekday()
+
     for i in weeklist:
         if week1 == i:
             week2 = weeklist[i]
-    d = ('{0:%Y}年{0:%m}月{0:%d}日'.format(now) + " " + week2)
-    t = ('{0:%P} {0:%I}時{0:%M}分{0:%S}秒'.format(now))
+            d = ('{0:%Y}年{0:%m}月{0:%d}日'.format(now) + " " + week2)
+            t = ('{0:%p} {0:%I}時{0:%M}分{0:%S}秒'.format(now))
 
     label1['text'] = d
     label2['text'] = t
-
+    
     root.after(100, clock)
 
 
@@ -34,7 +34,7 @@ label2 = tk.Label(root, font=("MSゴシック", "20", "bold"))
 label1.pack()
 label2.pack()
 
-#
+# datetimeで現在時刻を取得
 clock()
 
 # ウインドウを出力し続ける
